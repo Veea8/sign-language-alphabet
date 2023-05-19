f5cd9bb71658dafb2cf46b107d6ed493d7bec2e1
Veea8 <114304048+Veea8@users.noreply.github.com>
2023-05-19 20:02:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var ls = $(".letter-section")[0];
+Array.from({length: 26}, (_, i) => String.fromCharCode(i + 65)).forEach(i => {
+	var el = $("+div");
+	el.innerHTML = i;
+	el.on("click", e => {
+		if($("#active")) $("#active").removeAttribute("id");
+		el.id = "active";
+		$(".letter-image-section")[0].children[0].src = "img/hand_" + i + ".png";
+	});
+	ls.appendChild(el);
+});
+ls.children[0].click();
+
+var letterImagesContainer = document.getElementById("alphabet-overview-container");
+for (var j = 65; j <= 90; j++) {
+  var letter = String.fromCharCode(j);
+
+  var gridCell = document.createElement("div");
+  gridCell.classList.add("grid-cell");
+
+  var image = document.createElement("img");
+  image.src = "img/hand_" + letter + ".png";
+  gridCell.appendChild(image);
+
+  var letterSpan = document.createElement("span");
+  letterSpan.textContent = letter;
+  gridCell.appendChild(letterSpan);
+
+  letterImagesContainer.appendChild(gridCell);
+}
