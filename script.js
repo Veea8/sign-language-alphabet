abe17326230cdfa70e70024d80cb13e0f8c72dd6
Veea8 <veea81@gmail.com>
2023-11-01 14:37:16 +0000

    updated js

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-var ls = $(".letter-section")[0];
+var ls = document.querySelector(".letter-section");
+
 Array.from({length: 26}, (_, i) => String.fromCharCode(i + 65)).forEach(i => {
-	var el = $("+div");
+	var el = document.createElement("div");
 	el.innerHTML = i;
-	el.on("click", e => {
-		if($("#active")) $("#active").removeAttribute("id");
+
+	el.addEventListener("click", function(e) {
+		var activeEl = document.querySelector("#active");
+		if(activeEl) activeEl.removeAttribute("id");
 		el.id = "active";
-		$(".letter-image-section")[0].children[0].src = "img/hand_" + i.toLowerCase() + ".png";
+		document.querySelector(".letter-image-section").children[0].src = "img/hand_" + i.toLowerCase() + ".png";
 	});
+
 	ls.appendChild(el);
 });
 ls.children[0].click();
 
 var letterImagesContainer = document.getElementById("alphabet-overview-container");
 for (var j = 65; j <= 90; j++) {
-  var letter = String.fromCharCode(j);
+	var letter = String.fromCharCode(j);
 
-  var gridCell = document.createElement("div");
-  gridCell.classList.add("grid-cell");
+	var gridCell = document.createElement("div");
+	gridCell.classList.add("grid-cell");
 
-  var image = document.createElement("img");
-  image.src = "img/hand_" + letter.toLowerCase() + ".png";
-  gridCell.appendChild(image);
+	var image = document.createElement("img");
+	image.src = "img/hand_" + letter.toLowerCase() + ".png";
+	gridCell.appendChild(image);
 
-  var letterSpan = document.createElement("span");
-  letterSpan.textContent = letter;
-  gridCell.appendChild(letterSpan);
+	var letterSpan = document.createElement("span");
+	letterSpan.textContent = letter;
+	gridCell.appendChild(letterSpan);
 
-  letterImagesContainer.appendChild(gridCell);
+	letterImagesContainer.appendChild(gridCell);
 }
